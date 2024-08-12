afde1cc985aa6d6232e7f1e5182b748dfab01f0b
Mouloud <mouloudhai122@gmail.com>
2024-08-12 20:44:59 +0100

    use 'justifyContent' to NavBar styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none', padding: '10px', backgroundColor: '#333' }}>
+    <ul style={{ display: 'flex', gap: '1rem', justifyContent: "space-around", listStyle: 'none', padding: '10px', backgroundColor: '#333' }}>
       <li><Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link></li>
       <li><Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link></li>
       <li><Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link></li>
