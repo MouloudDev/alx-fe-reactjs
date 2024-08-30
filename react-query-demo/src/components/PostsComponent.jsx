0a5ac0769566984bafa50305aa278dcf428cf133
Mouloud <mouloudhai122@gmail.com>
2024-08-30 19:05:32 +0100

    Add caching options

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,12 +1,18 @@
 import { useQuery } from 'react-query';
-
 const fetchPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     return res.json();
 };
 
 function PostsComponent() {
-    const { data, isError, isLoading, refetch } = useQuery('fetchPosts', fetchPosts);
+    const queryOptions = {
+        cacheTime: 6000,
+        staleTime: 1000,
+        refetchOnWindowFocus: false,
+        keepPreviousData : true
+    }
+    const { data, isError, isLoading, refetch } =
+      useQuery('fetchPosts', fetchPosts, queryOptions);
 
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error: error loading posts.</div>;
