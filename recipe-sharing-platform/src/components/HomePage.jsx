54ccf5f9eff130ffd86ef95272822b3dbe0e4e42
Mouloud <mouloudhai122@gmail.com>
2024-09-11 21:09:20 +0100

    add 'text' style to HomePage component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
     if (!data) return <div>Loading...</div>;
 
     return (
-        <div className="grid grid-cols-1 shadow-sm shadow-orange-200 hover:shadow-lg rounded-md p-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 shadow-sm shadow-orange-200 hover:shadow-lg rounded-md p-3 text-base sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {data.map(item => <RecipeCard recipe={item} key={item.id}/>)}
         </div>
     )
